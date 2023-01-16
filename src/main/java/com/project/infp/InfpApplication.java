bb81cbc46ab865a93ff67b08d726ec16102dbc1c
leedonghu <ldh930517@gmail.com>
2023-01-16 23:29:54 +0900

    2023-01-16

---
+++ src/main/java/com/project/infp/InfpApplication.java
@@ -0,0 +1,13 @@
+package com.project.infp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/infp/InfpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.infp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
